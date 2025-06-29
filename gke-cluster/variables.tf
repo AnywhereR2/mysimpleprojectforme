2e0f4ed8b8a17aceeea499b5b14dd64a9f7f338f
Anton Grigoryev <anywhere05041993@gmail.com>
2025-06-30 00:30:19 +0300

    test
    222

---
+++ gke-cluster/variables.tf
@@ -0,0 +1,45 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-east4"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-east4-a"
+}
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Type of GCE VM instances"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "disk_size" {
+  description = "Size of boot disk (GB)"
+  type        = number
+  default     = 10
+}
+
+variable "min_node" {
+  description = "Minimum number of nodes in the pool"
+  type        = number
+  default     = 1
+}
+
+variable "max_node" {
+  description = "Maximum number of nodes in the pool"
+  type        = number
+  default     = 1
+}
